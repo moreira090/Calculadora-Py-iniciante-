ade973cc23d92883cc378327e7faaebaf8395fd6
Davi Moreira <moreiradavi925@gmail.com>
2025-03-18 21:08:42 -0300

    :boom: commit inicial

---
+++ CalculadoraComIF.py
@@ -0,0 +1,18 @@
+#calculadora
+a = ((float(input('escreva um numero:'))))
+op =(input('escolha uma operação'))
+b = ((float(input('escolha outro numero:'))))
+res = a + b 
+if op == '+':
+   res=a+b
+elif op == '-':
+   res=a-b
+elif op == 'x':
+   res=a*b
+elif op == '/':
+   res=a/b
+else:
+   print('Operador Invalido ')   
+
+
+print(a+ op + b + ' =' + res ); 
